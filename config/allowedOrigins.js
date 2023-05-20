23f0c14d3eb6523137aa591e2781a21394a9a430
Pratyush Kumar <pratyushkr4813@outlook.com>
2023-05-20 18:51:10 +0530

    fix

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
-    'https://copyright-app.onrender.com'];
+    'https://copyright-app.onrender.com',
+    'https://kopyrightit.onrender.com'];
 
 module.exports = allowedOrigins;
